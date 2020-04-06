3a0b24580df22e4fbae3dedbc4b10835661a8399
mvkvinod <vinodgmkumar13@outlook.com>
2020-04-06 16:33:52 +0530

    Configure and create helpers - Change include_all_helpers config to false - Split the _collapsible_elements.html.erb file's content into   partials and extract logic from the file into partials

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module TddSampleApplication
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    #to avoid including of all helper moules in views
+    config.action_controller.include_all_helpers = false
   end
 end
