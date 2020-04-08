1fdebca7ddb05e05cc8cb2f06785fa81779c3a3e
mvkvinod <vinodgmkumar13@outlook.com>
2020-04-08 12:33:00 +0530

    Added specs to NavigationHrlper's collapsible_links_partial_path method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -88,8 +88,8 @@ require 'capybara/poltergeist'
 require 'factory_girl_rails'
 require 'capybara/rspec'
 
-config.include Devise::Test::IntergrationHelpers, type: :feature
-config.include FactoryGirl::Syntax::methods
+config.include Devise::Test::IntegrationHelpers, type: :feature
+config.include FactoryGirl::Syntax::Methods
 Capybara.javascript_driver = :poltergeist
 Capybara.server = :puma
 
